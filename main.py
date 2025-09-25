e029c417faedc76d62d60902f5e290a23999ceeb
Pam_silvah <senai202v@gmail.com>
2025-09-25 17:10:24 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,60 +1,51 @@
-# sistema de mercado
+
+#Exercício
 
 
-print('MERCADO Z')
-produtos = ['','1 arroz',' 2 feijão',' 3 macarrão',' 4 leite',' 5 chocolate']
-valores = [0,10.0, 15.0, 5.0, 8.50,11.25]
-carrinho = []
-meu_total = []
+#Exercício 0:** Escreva um programa que use a função
+#`range()` para gerar os números pares de 2 a 20 e, em
+#seguida, imprima cada número.
 
 
-pedir = input('Deseja pedir?')
-pedido = pedir == 'sim'
-print('Resposta:',pedido)
+print(list(range(2,21,2)))
 
 
-# escolhendo o produto
-print(produtos)
+#**Exercício 1:** Crie uma lista chamada numeros que
+#contenha os números inteiros de 1 a 10 e imprima-a na tela.
+
+numeros = [1,2,3,4,5,6,7,8,9,10]
+print(numeros)
+
+#*Exercício 2:** Acesse e imprima o terceiro elemento da
+#lista `numeros`.
+
+terceiro = numeros[2]
+print(terceiro)
 
 
-produto_1 = int(input('Digite o Id 1 2 3 4 do produto: '))
-produto_2 = int(input('Digite o Id 1 2 3 4 do produto: '))
-produto_3 = int(input('Digite o Id 1 2 3 4 do produto: '))
-produto_4 = int(input('Digite o Id 1 2 3 4 do produto: '))
+#Exercício 3:** Adicione o número 9 à lista `numeros` e
+#imprima a lista atualizada.
 
-
-# add ao carrinho
-carrinho.append(produtos[produto_1])
-carrinho.append(produtos[produto_2])
-carrinho.append(produtos[produto_3])
-carrinho.append(produtos[produto_4])
-
-
-# valores dos produtos estão entrando na lista
-meu_total.append(valores[produto_1])
-meu_total.append(valores[produto_2])
-meu_total.append(valores[produto_3])
-meu_total.append(valores[produto_4])
-
-
-soma = sum(meu_total)
-
-
-print('Seu carrinho', carrinho)
-print('Total R$', soma)
-
-
-print('Escolha a forma de pagamento')
-
-
-pagamentos  = ['id  0   PIX',' id 1  CC','id  2  CD']
-
-print(pagamentos)
-escolha = int(input('Escolha a forma: '))
-print('Sua froma de pagamento é:', pagamentos [escolha])
-print('Obrigada Volte sempre!')
+numeros.append(9)
+print(numeros)
 
 
 
+#**Exercício 4:** Remova o número 5 da lista `numeros` e
+#imprima a lista resultante.
 
+numeros.remove(5)
+print(numeros)
+
+#**Exercício 5:** Crie uma lista chamada carros contendo
+#três nomes de carros diferentes. Em seguida, concatene
+#essa lista com a lista `numeros` e imprima o resultado.
+
+carros = ['ferrari','jeep','mustang']
+print(carros, numeros)
+
+carros.extend(numeros)
+print(carros)
+
+          
 
